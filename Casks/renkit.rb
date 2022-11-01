8e55bffc28ad1291087faa6afb974d95ecd7969c
cobaltcore <cobaltcore@yandex.com>
2022-11-01 22:21:52 +0100

    Add rcodesign cask as dependency of renkit

--- Casks/renkit.rb
+++ Casks/renkit.rb
@@ -5,6 +5,8 @@ cask "renkit" do
   sha256 arm:   "fba006c76a369f0b2a291cf9b54268ffac18f2b92faed34d7570598f6fffad5e",
          intel: "5771c5a72b3400d1ee12400c9dfb3df18b8f72fdc9ccc9df6bedf5290c65adc0"
 
+  depends_on cask: "rcodesign"
+
   url "https://github.com/kobaltcore/renkit/releases/download/v#{version}/renkit-macos-#{arch}.zip"
   name "renkit"
   desc "Collection of tools to help you use Ren'Py instances from the command-line"

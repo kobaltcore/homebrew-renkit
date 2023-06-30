9476701f1ed6999a9c313fdbcbcd3302da5d5fe4
cobaltcore <cobaltcore@yandex.com>
2023-06-30 20:22:44 +0200

    Update cask

--- Casks/renkit.rb
+++ Casks/renkit.rb
@@ -1,9 +1,9 @@
 cask "renkit" do
   arch arm: "arm64", intel: "amd64"
 
-  version "3.3.0"
-  sha256 arm:   "5bd2a01ed6a5dc83dfcbcd7b19c903b36ba81bf49a1b94acd24fc383517070a2",
-         intel: "72a66b3f59e18c0b62d117487e27edc54d2e0569e31ec4f5cf2e0bba33dd3e7c"
+  version "3.3.1"
+  sha256 arm:   "75fefc60913e69d59a25157a5af1a617a2645802c35f68aa0b85d5ae401c98f0",
+         intel: "391e79ecb910d534fc99c1c580db5984e3287e3fd23846901406b09ed162db07"
 
   url "https://github.com/kobaltcore/renkit/releases/download/v#{version}/renkit-macos-#{arch}.tar.gz"
   name "renkit"
